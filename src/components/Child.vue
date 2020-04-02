261315b297a48f0486450ee7ee6b85dfcbbbbb17
nada1004tj <nada1004tj@naver.com>
2020-04-02 11:46:36 +0900

    first commint

---
+++ src/components/Child.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+       <child_pomponents v-on:child="parents"></child_pomponents>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import child_pomponents from '@/components/child_pomponents'
+
+export default {
+  methods: {
+    parents(message){
+      alert(message);
+    }
+  },
+  components: {
+    child_pomponents
+  }
+}
+</script>
---
+++ src/components/child_pomponents.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="yellow lighten-3 pa-3">
+    <h3>회원 정보 수정</h3>
+    <p>수정내용</p>
+
+    <button @click="childfunction">수정1</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+      return{
+        user:{}
+      }
+  },
+  created() {
+    this.user = "child user"
+  },
+  methods: {
+    childfunction: function() {
+       this.$emit("child",this.user)
+    }
+  }
+
+}
+</script>
